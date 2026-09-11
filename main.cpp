17c1ce707292a58258fddc868f1c53db0ab23dea
nathandebarros <nathanbartolomeu@gmail.com>
2026-09-11 00:36:42 -0300

    Base do projeto

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Character{
+    public:
+        int hp;
+        int xp;
+        int charisma;
+};
---
+++ raca.hpp
@@ -0,0 +1,10 @@
+#ifndef RACA_H
+#define RACA_H
+
+class Human{
+    public:
+        std:string nome;
+};
+
+
+#endif
