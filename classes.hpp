b753b2e79ea465222f8e6b98b5c7abea92c49c07
nathandebarros <nathanbartolomeu@gmail.com>
2026-09-13 22:23:32 -0300

    Adicionando Classes
    Fiz a adição de algumas classes e exclui um cabeçalho sem utilidade até o momento.

--- classes.hpp
+++ classes.hpp
@@ -9,37 +9,102 @@
 using namespace std;
 class Character{
     public:
+        std::string Name;
         int hp;
         int xp;
-        int stregth;
+        int strength;
         int dexterity;
-        int constituition;
+        int constitution;
         int intelligence;
         int wisdom;
         int charisma;
 
-        int Attack();
-        int Defense();
+        void Attack(){
+            cout << "ataque básico";
+        };
+        void Defense() {
+            cout << "defesa";
+        };
 };
 
 class Equip{
     public:
-        std::string nome;
-        std::string type;
+        std::string Name;
+        std::string Type;
 };
 
-class Assistent:Character{
+class Class{
+    public:
+        std::string className;
+
+};
+
+class Race{
+    public:
+        std::string raceName;
+
+};
+
+class Loot{
+    public:
+        std::string lootName;
+        std::string lootType;
+};
+
+class Assistant:Character{
 
 };
 
 class Player: Character{
     public:
-        Equip equipamento;
+        Class playerClass;
+        Race playerRace;
+        Equip Weapon;
+        Equip Armor;
+
+        void Attack() {
+            cout << Name << " usou " << Weapon.Name << " e desferiu um golpe\n";
+        };
+
+        void Defense(){
+            cout << Name << " usou " << Armor.Name << " para se defender\n";
+        };
+        
 };
 
-class Boss: Character{
+class Enemy: public Character{
     public:
-        int SpecialAttack();
+        Loot itemDrop;
+        Equip Weapon;
+        Equip Armor;
+
+};
+
+class Minions:Enemy{
+    public:
+        void Attack() {
+            cout << Name << " usou " << Weapon.Name << " e desferiu um golpe\n";
+        };
+
+        void Defense(){
+            cout << Name << " usou " << Armor.Name << " para se defender\n";
+        };
+
+};
+
+class Boss: public Enemy{
+    public:
+        void Attack(){
+            cout << Name << " usou " << Weapon.Name << " e desferiu um ataque básico\n";
+        };
+
+        void Defense(){
+            cout << Name <<" usou " << Armor.Name << "para defender\n";
+        };
+
+        void SpecialAttack(){
+            cout << Name << "Usou " << Weapon.Name << " e desferiu um ataque avassalador\n";
+        };
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 using namespace std;
 
 
-class Character{
-    public:
-        int hp;
-        int xp;
-        int charisma;
-};
+int main(){
+
+
+    return 0;
+}
